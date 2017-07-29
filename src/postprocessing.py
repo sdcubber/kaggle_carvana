117e03cdc78cc05c74594ba712b3181b5e2eb503
stijndc <stijn.decubber@ugent.be>
2017-07-29 09:43:23 +0200

    Code for run-lenght encoding

--- src/postprocessing.py
+++ src/postprocessing.py
@@ -1,10 +1,12 @@
-# Import libraries
-import numpy as np # linear algebra
+# Script for postprocessing functions, like making a submission file etc.
+
+import numpy as np
 from PIL import Image
 
 def rle(img):
     """Fast run-lenght encoding of an image.
     See https://www.kaggle.com/hackerpoet/even-faster-run-length-encoder
+    And https://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
     Input
     -----
     img: image to be encoded
@@ -13,6 +15,7 @@ def rle(img):
     -------
     start_ix: start indices
     lengths: lengths
+    rle_str: resulting rle encoding as a string
     """
     flat_img = img.flatten()
     flat_img = np.where(flat_img > 0.5, 1, 0).astype(np.uint8)
@@ -23,4 +26,10 @@ def rle(img):
     ends_ix = np.where(ends)[0] + 2
     lengths = ends_ix - starts_ix
 
-    return starts_ix, lengths
+    result = result = [None]*(len(starts_ix)+len(lengths))
+    result[::2] = starts_ix
+    result[1::2] = lengths
+    result = [str(e) for e in result]
+
+    rle_str = ' '.join(result)
+    return starts_ix, lengths, rle_str

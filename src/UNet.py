0d8043a38f616705a57b524a93452813b21558df
sdcubber <stijn.decubber@ugent.be>
2017-08-02 14:14:30 +0200

    Added code to upscale test set predictions

--- src/UNet.py
+++ src/UNet.py
@@ -86,7 +86,7 @@ def run_script(name, epochs, batch_size, debug):
     rle_encoded_predictions_all = [j for batch in rle_encoded_predictions for j in batch]
 
     sub = pd.DataFrame({'img': test_idx_all, 'rle_mask': rle_encoded_predictions_all})
-    sub.to_csv('../predictions/test/{}.csv'.format(name))
+    sub.to_csv('../predictions/test/{}.csv'.format(name), index=False)
 
 
 def main():
--- src/processing/processing_utils.py
+++ src/processing/processing_utils.py
@@ -3,13 +3,33 @@
 import numpy as np
 from PIL import Image
 
-def rle(img):
+def upscale_test_img(pil_img):
+    """Upscale PIL Image to the shape of the test images.
+    Args
+    pil_img: PIL Image"""
+
+    # Go to square of size (1280,1280) with bilinear interpolation
+    im = pil_img.resize((1280,1280), resample=Image.BILINEAR)
+    # Go to numpy
+    im = np.array(im)/255
+
+    # Pad with zeros to a width of 1910
+    # See https://docs.scipy.org/doc/numpy/reference/generated/numpy.pad.html
+    n_padding = (1910 - 1280)// 2
+    im = np.pad(im,((0,0),(n_padding,n_padding)), 'constant', constant_values=(0))
+
+    return(im)
+
+    
+
+def rle(img, threshold=0.5):
     """Fast run-lenght encoding of an image.
     See https://www.kaggle.com/hackerpoet/even-faster-run-length-encoder
     And https://stackoverflow.com/questions/3678869/pythonic-way-to-combine-two-lists-in-an-alternating-fashion
     Input
     -----
     img: image to be encoded
+    threshold: cut-off probability to assign a pixel to class 1
 
     Returns
     -------
@@ -18,7 +38,7 @@ def rle(img):
     rle_str: resulting rle encoding as a string
     """
     flat_img = img.flatten()
-    flat_img = np.where(flat_img > 0.5, 1, 0).astype(np.uint8)
+    flat_img = np.where(flat_img > threshold, 1, 0).astype(np.uint8)
 
     starts = np.array((flat_img[:-1] == 0) & (flat_img[1:] == 1))
     ends = np.array((flat_img[:-1] == 1) & (flat_img[1:] == 0))

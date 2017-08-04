89fe39a470b98a3e9fb68079e47d81e0928cc83a
stijndc <stijn.decubber@ugent.be>
2017-08-04 09:57:38 +0200

    Gzip sumbission files in jobscript

--- jobscripts/stijn_test.sh
+++ jobscripts/stijn_test.sh
@@ -5,11 +5,15 @@
 source activate pytorch
 
 # Assign a name for each script. Output will be logged under that name
-NAME='UNet_256'
+NAME='test'
 
 cd ../src
 echo 'Running: '$NAME
-python UNet.py $NAME 25 256 -b 16 2>&1 | tee ../jobscripts/logs/${NAME}
+python UNet.py $NAME 1 128 -b 32 -db 2>&1 | tee ../jobscripts/logs/${NAME}
+
+echo 'Gzipping submission file...'
+gzip ../predictions/test/${NAME}.csv ../predictions/test/${NAME}.csv.gz
+
 echo 'Done.'
 
 cd ../jobscripts/logs

249c362f75348c5f9c48f5b21fa7605d5ff60bfa
Bac Nguyen Cong <bac.nguyencong@ugent.be>
2017-08-07 22:34:38 +0200

    Fixed encoded rle

--- src/processing/processing_utils.py
+++ src/processing/processing_utils.py
@@ -39,36 +39,43 @@ def upscale_test_img(pil_img, crop=False):
 
     if crop:
         # Go to square of size (1280,1280) with bilinear interpolation
-        im = pil_img.resize((1280,1280), resample=Image.BILINEAR)
+        im = pil_img.resize((1280, 1280), resample=Image.BILINEAR)
         # Go to numpy
         im = np.array(im) // 255
         # Pad with zeros to a width of 1918
         # See https://docs.scipy.org/doc/numpy/reference/generated/numpy.pad.html
-        n_padding = (1918 - 1280)// 2
-        im = np.pad(im,((0,0),(n_padding,n_padding)), 'constant', constant_values=(0))
+        n_padding = (1918 - 1280) // 2
+        im = np.pad(im, ((0, 0), (n_padding, n_padding)), 'constant', constant_values=(0))
 
     else:
         # Go to original resolution by upscaling, no padding required since no cropping was done
-        im = pil_img.resize((1918,1280), resample=Image.ANTIALIAS)
+        im = pil_img.resize((1918, 1280), resample=Image.ANTIALIAS)
         im = np.array(im) // 255
 
-    return(im)
+    return (im)
+
 
 def rle(img):
     """
-    This function is not correct....
     img: numpy array, 1 - mask, 0 - background
     Returns run length as string formated
     """
     pixels = img.flatten()
-    pixels[0] = 0
-    pixels[-1] = 0
-    runs = np.where(pixels[1:] != pixels[:-1])[0] + 2
-    runs[1::2] = runs[1::2] - runs[:-1:2]
+    j, i, n = 0, 0, len(pixels)
+    runs = []
+
+    while i < n:
+        while i < n and pixels[i] == 0: i += 1
+        b = i
+        while i < n and pixels[i] == 1: i += 1
+        e = i
+        if b != e:
+            runs.extend([b + 1, e - b])
 
     return ' '.join(str(x) for x in runs)
 
-def rle_decode(im_rle, shape=(1280,1918)):
+
+def rle_decode(im_rle, shape=(1280, 1918)):
     """
     im_rle: run-length encoded image
     shape: (height,width) of array to return
@@ -79,7 +86,7 @@ def rle_decode(im_rle, shape=(1280,1918)):
     starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
     starts -= 1
     ends = starts + lengths
-    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    img = np.zeros(shape[0] * shape[1], dtype=np.uint8)
     for lo, hi in zip(starts, ends):
         img[lo:hi] = 1
 
@@ -93,7 +100,7 @@ def read_mask_image(car_code, angle_code):
     angle_code: code of the angle
     """
     mask_img_path = os.path.join(TRAIN_MASKS_PATH, car_code + '_' + angle_code + '_mask.gif')
-    mask_img = np.array(Image.open(mask_img_path)) #.convert(mode='L'))
+    mask_img = np.array(Image.open(mask_img_path))  # .convert(mode='L'))
 
     return mask_img
 
@@ -105,6 +112,7 @@ def show_mask_image(car_code, angle_code):
     mask_img = read_mask_image(car_code, angle_code)
     plt.imshow(mask_img, cmap='Greys_r')
     plt.show()
+
 
 def rle_to_string(codes):
     """
@@ -120,22 +128,25 @@ def train_valid_split(csvfile, rotation_ids=range(1, 17), valid=0.1):
     # get all files with specific rotations in rotation_ids
     rotation_ids = np.char.mod('_%02d', rotation_ids)
     im_list = np.array([item for item in im_list
-                    if any(rot_id in item for rot_id in rotation_ids)])
+                        if any(rot_id in item for rot_id in rotation_ids)])
 
-    #random shuffle the indices
+    # random shuffle the indices
     np.random.shuffle(im_list)
-    t_size = int(im_list.shape[0] * (1-valid))
+    t_size = int(im_list.shape[0] * (1 - valid))
 
     return im_list[:t_size], im_list[t_size:]
+
 
 def update_spreadsheet(timestamp, im_size, arch, epochs, best_dice, best_loss, modelname, rotation):
     """Update the spreadsheet with information from this experiment"""
     # TODO
 
-    return(True)
+    return (True)
+
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
+
     def __init__(self):
         self.reset()
 
@@ -154,16 +165,17 @@ class AverageMeter(object):
 
 class Logger(object):
     """ Write log info to file and to standard output  """
+
     def __init__(self):
-        self.terminal = sys.stdout  #stdout
+        self.terminal = sys.stdout  # stdout
         self.file = None
 
     def open(self, file, mode=None):
-        if mode is None: mode ='w'
+        if mode is None: mode = 'w'
         self.file = open(file, mode)
 
     def write(self, message: object, is_terminal: object = 1, is_file: object = 1) -> object:
-        if '\r' in message: is_file=0
+        if '\r' in message: is_file = 0
 
         if is_terminal == 1:
             self.terminal.write(message)

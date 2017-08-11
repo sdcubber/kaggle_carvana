8a2960953cc22ec32eb02eee8c25ad74cf280f05
Bac Nguyen Cong <bac.nguyencong@ugent.be>
2017-08-11 12:23:39 +0200

    add totensor and read functions

--- src/processing/augmentation.py
+++ src/processing/augmentation.py
@@ -5,3 +5,28 @@ from collections import deque
 from PIL import Image
 import cv2
 from data.config import *
+
+
+def imread_cv2(image_path):
+    """ 
+    Read image_path with cv2 format (H, W, C)
+    if image is '.gif' outputs is a numpy array of {0,1}
+    """
+    image_format = image_path[-3:]
+    if image_format == 'jpg':
+        image = cv2.imread(image_path)
+    else:
+        image = np.array(Image.open(image_path))
+        image = image.reshape(image.shape[0], image.shape[1], 1)
+
+    return image
+
+
+def image_to_tensor(image, mean=0, std=1.):
+    """Transform image (input is numpy array, read in by cv2) """
+    image = image.astype(np.float32)
+    image = (image-mean)/std
+    image = image.transpose((2,0,1))
+    tensor = torch.from_numpy(image)
+
+    return tensor
